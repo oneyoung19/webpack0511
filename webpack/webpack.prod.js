c41c1c65de27fe0702e9681830dfe38adfd36b8d
yangxiaoping <2878847069@qq.com>
2020-05-11 20:29:26 +0800

    feat:添加optimize-css-assets-webpack-plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,9 +3,13 @@ const common = require('./webpack.common')
 
 // 自动清除dist目录
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+// 压缩从js文件中分离出来的css
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = merge(common, {
+  // mode有什么用？这里即使设置了production，可分离的css并没有压缩，还是需要手动设置optimize-css-assets-webpack-plugin插件
+  // mode: 'production',
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new OptimizeCssAssetsWebpackPlugin()
   ]
 })
